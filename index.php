3715d7a8b0441a9804950a56f6312bf04dd797fc
jossday <undergroundempire7@gmail.com>
2016-07-25 23:01:50 +0200

    Implémentation interface login

--- index.php
+++ index.php
@@ -12,14 +12,35 @@
      <link title="timeline-styles" rel="stylesheet" href="https://cdn.knightlab.com/libs/timeline3/latest/css/timeline.css">
      <link title="timeline-styles" rel="stylesheet" href="css/style.css">
      <script src="https://cdn.knightlab.com/libs/timeline3/latest/js/timeline.js"></script>
+     <script src="js/jquery.js"></script>
   </head>
   <body>
       <div id="header">
-        <a href="index.php">
+        <div id="title">
           <h1>ChronoGramm</h1>
           <h3>L'actualité d'un coup d'oeil</h3>
-        </a>
+        </div>
+        <div id="login">
+          <a href="#" id="loginLink" class="loginLink">Login</a>
+          <a href="#" id="register" class="loginLink">Register</a>
+        </div>
       </div>
+
+      <form action="" method="post" id="loginForm">
+        <div class="form-group">
+          <label class="fa fa-user" for="login__username"><span>Login</span></label>
+          <input type="text" placeholder="Identifiant" id="Login" class="form-control" required/>
+        </div>
+        <div class="form-group">
+          <label class="fa fa-lock" for="login__password"><span>Password</span></label>
+          <input type="password" placeholder="Password" id="mdp" class="form-control" required />
+          <a href="recuperation.php" id="oubliMdp">Forget ?</a>
+        </div>
+        <div class="form-group">
+          <button type="submit" class="submit btn btn-success" id="btnConnexion">Connect</button>
+        </div>
+      </form>
+
       <div id='timeline-embed' style="width: 100%; height: 800px"></div>
       <?php
       require('traitements/getEvents.php');
@@ -33,6 +54,13 @@
         console.log(data);
         timeline = new TL.Timeline('timeline-embed',
           'traitements/timeline.json', additionalOptions);
+
+        $("#title").click(function(){
+          window.location = "index.php";
+        });
+        $("#loginLink").click(function(){
+          $("#loginForm").fadeIn(700);
+        });
       </script>
   </body>
 </html>
