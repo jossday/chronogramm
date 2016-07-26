bcb9f05036ec2e1b15ae48981a5b7aaa8d6312fd
jossday <undergroundempire7@gmail.com>
2016-07-26 10:42:41 +0200

    Login fonctionnel

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -20,16 +23,27 @@
           <h1>ChronoGramm</h1>
           <h3>L'actualité d'un coup d'oeil</h3>
         </div>
-        <div id="login">
-          <a href="#" id="loginLink" class="loginLink">Login</a>
-          <a href="#" id="register" class="loginLink">Register</a>
-        </div>
+        <?php
+        if((isset($_SESSION['login'])) && ($_SESSION['login'] != '')){
+          echo '  <div id="loginZone">
+                    <span id="loginName">'.$_SESSION['login'].'</span>
+                    <a href="#" id="myAccount" class="loginLink">My Account</a>
+                    <a href="#" id="addEvent" class="loginLink">Add a new Event</a>
+                  </div>';
+        }
+        else{
+          echo '  <div id="loginZone">
+                    <a href="#" id="loginLink" class="loginLink">Login</a>
+                    <a href="#" id="register" class="loginLink">Register</a>
+                  </div>';
+        }
+        ?>
       </div>
 
       <form action="" method="post" id="loginForm">
         <div class="form-group">
           <label class="fa fa-user" for="login__username"><span>Login</span></label>
-          <input type="text" placeholder="Identifiant" id="Login" class="form-control" required/>
+          <input type="text" placeholder="Identifiant" id="login" class="form-control" required/>
         </div>
         <div class="form-group">
           <label class="fa fa-lock" for="login__password"><span>Password</span></label>
@@ -37,7 +51,7 @@
           <a href="recuperation.php" id="oubliMdp">Forget ?</a>
         </div>
         <div class="form-group">
-          <button type="submit" class="submit btn btn-success" id="btnConnexion">Connect</button>
+          <button type="submit" class="submit btn btn-success" id="btnConnect">Connect</button>
         </div>
       </form>
 
@@ -60,6 +74,36 @@
         });
         $("#loginLink").click(function(){
           $("#loginForm").fadeIn(700);
+        });
+        //Connexion
+        $("#btnConnect").click(function(){
+                var login = $('#login').val();
+                var pass = $("#mdp").val();
+                $.ajax({
+                type: "POST",
+                url: "traitements/connect.php",
+                data: {login:login, pass:pass}, //your form data to post goes here as a json object
+                dataType: "html",
+                success: function(msg){
+                  console.log($.trim(msg));
+                    if($.trim(msg) == 0){
+                        $("#result").html("<span class='alert-danger'>Cet identifiant n'existe pas.</span>");
+                    }
+                    else if($.trim(msg) == 2){
+                        $("#result").html("<span class='alert-danger'>Vous avez essayé de vous connecter 3 fois sans succès. Veuillez réessayer plus tard ou contacter le support.</span>");
+                    }
+                    else if($.trim(msg) == 3){
+                        $("#result").html("<span class='alert-danger'>Mot de passe incorrect.</span>");
+                    }
+                    else if($.trim(msg) == 4){
+                        $("#result").html("<span class='alert-danger'>Veuillez remplir tous les champs.</span>");
+                    }
+                    else{
+                      window.location = "index.php";
+                    }
+                }
+            });
+        return false;
         });
       </script>
   </body>
