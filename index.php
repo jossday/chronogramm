3d919a3136118883fc7c4f3eb4d06f01f77374df
jossday <undergroundempire7@gmail.com>
2016-07-30 09:58:32 +0200

    Inscription fonctionnelle

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ session_start();
         else{
           echo '  <div id="loginZone">
                     <a href="#" id="loginLink" class="loginLink">Login</a>
-                    <a href="#" id="register" class="loginLink">Register</a>
+                    <a href="#" id="registerLink" class="loginLink">Register</a>
                   </div>';
         }
         ?>
@@ -40,7 +40,7 @@ session_start();
       <form action="" method="post" id="loginForm">
         <div class="form-group">
           <label class="fa fa-user" for="login__username"><span>Login</span></label>
-          <input type="text" placeholder="Identifiant" id="login" class="form-control" required/>
+          <input type="text" placeholder="Login" id="login" class="form-control" required/>
         </div>
         <div class="form-group">
           <label class="fa fa-lock" for="login__password"><span>Password</span></label>
@@ -50,6 +50,34 @@ session_start();
         <div class="form-group">
           <button type="submit" class="submit btn btn-success" id="btnConnect">Connect</button>
         </div>
+        <div id="resultLogin"></div>
+      </form>
+
+      <form action="" method="post" id="registerForm">
+        <div class="form-group">
+          <label class="fa fa-user" for="register__username"><span>Login</span></label>
+          <input type="text" placeholder="Login" id="loginRegister" class="form-control" required/>
+        </div>
+        <div class="form-group">
+          <label class="fa fa-lock" for="register__password"><span>Password</span></label>
+          <input type="password" placeholder="Password" id="mdpRegister" class="form-control" required />
+        </div>
+        <div class="form-group">
+          <label class="fa fa-lock" for="register__password__confirmation"><span>Password Confirmation</span></label>
+          <input type="password" placeholder="Password Confirmation" id="mdpConfirm" class="form-control" required />
+        </div>
+        <div class="form-group">
+          <label class="fa fa-lock" for="register__email"><span>Email</span></label>
+          <input type="email" placeholder="Email" id="email" class="form-control" required />
+        </div>
+        <div class="form-group">
+          <label class="fa fa-lock" for="register__birth"><span>Birth Date</span></label>
+          <input type="date" placeholder="Birth Date" id="birth" class="form-control" required />
+        </div>
+        <div class="form-group">
+          <button type="submit" class="submit btn btn-success" id="btnRegister">Register</button>
+        </div>
+        <div id="resultRegister"></div>
       </form>
 
       <form id="search" name="search" method="get" action="">
@@ -58,9 +86,14 @@ session_start();
 
       <div id='timeline-embed' style="width: 100%; height: 800px"></div>
       <?php
-      require('traitements/getEvents.php');
+      $expireTime = time()-1800; //Expiration fixée à 30 minutes
+      $cacheFile = "traitements/timeline.json";
+      if(filemtime($cacheFile) < $expireTime){ //Si le cache est expiré on recharge les données
+        require('traitements/getEvents.php');
+      }
       ?>
       <script type="text/javascript">
+        //Initialisation de la timeline
         var additionalOptions = {
             start_at_slide: 0,
             debug:true
@@ -68,12 +101,35 @@ session_start();
         timeline = new TL.Timeline('timeline-embed',
           'traitements/timeline.json', additionalOptions);
 
+        //Lien artificiel du titre vers page d'accueil
         $("#title").click(function(){
           window.location = "index.php";
         });
+
+        //Affichage du formulaire de connexion
         $("#loginLink").click(function(){
           $("#loginForm").fadeIn(700);
+          //Overlay noir
+          $('body').append('<div id="fade"></div>');
+          $('#fade').show();
         });
+
+        //Affichage du formulaire d'inscription
+        $('#registerLink').click(function(){
+          $("#registerForm").fadeIn(700);
+          //Overlay noir
+          $('body').append('<div id="fade"></div>');
+          $('#fade').show();
+        });
+
+        /*$('body').click(function(e){
+          var target = $(e.target);
+          if ((target.not('#loginLink')) && (target.not('#registerLink'))){
+              $("#fade").remove();
+              $("#loginForm").hide();
+              $("#registerForm").hide();
+          }
+        });*/
         //Connexion
         $("#btnConnect").click(function(){
                 var login = $('#login').val();
@@ -81,29 +137,64 @@ session_start();
                 $.ajax({
                 type: "POST",
                 url: "traitements/connect.php",
-                data: {login:login, pass:pass}, //your form data to post goes here as a json object
+                data: {login:login, pass:pass},
                 dataType: "html",
                 success: function(msg){
                   console.log($.trim(msg));
                     if($.trim(msg) == 0){
-                        $("#result").html("<span class='alert-danger'>Cet identifiant n'existe pas.</span>");
+                        $("#resultLogin").html("<span class='alert-danger'>Cet identifiant n'existe pas.</span>");
                     }
                     else if($.trim(msg) == 2){
-                        $("#result").html("<span class='alert-danger'>Vous avez essayé de vous connecter 3 fois sans succès. Veuillez réessayer plus tard ou contacter le support.</span>");
+                        $("#resultLogin").html("<span class='alert-danger'>Vous avez essayé de vous connecter 3 fois sans succès. Veuillez réessayer plus tard ou contacter le support.</span>");
                     }
                     else if($.trim(msg) == 3){
-                        $("#result").html("<span class='alert-danger'>Mot de passe incorrect.</span>");
+                        $("#resultLogin").html("<span class='alert-danger'>Mot de passe incorrect.</span>");
                     }
                     else if($.trim(msg) == 4){
-                        $("#result").html("<span class='alert-danger'>Veuillez remplir tous les champs.</span>");
+                        $("#resultLogin").html("<span class='alert-danger'>Veuillez remplir tous les champs.</span>");
                     }
                     else{
-                      window.location = "index.php";
+                      location.reload();
                     }
                 }
             });
         return false;
         });
+        //Inscription
+        $("#btnRegister").click(function(){
+          var login = $('#loginRegister').val();
+          var pass = $('#mdpRegister').val();
+          var confirm = $("#mdpConfirm").val();
+          var email = $("#email").val();
+          var birth = $("#birth").val();
+          $.ajax({
+          type: "POST",
+          url: "traitements/register.php",
+          data: {login:login, pass:pass, confirm:confirm, email:email, birth:birth},
+          dataType: "html",
+            success: function(msg){
+              console.log($.trim(msg));
+                if($.trim(msg) == 0){
+                    $("#resultRegister").html("<span class='alert-danger'>Please fill all the fields.</span>");
+                }
+                else if($.trim(msg) == 2){
+                    $("#resultRegister").html("<span class='alert-danger'>This login is already in use.</span>");
+                }
+                else if($.trim(msg) == 3){
+                    $("#resultRegister").html("<span class='alert-danger'>Password is not valid.</span>");
+                }
+                else if($.trim(msg) == 4){
+                    $("#resultRegister").html("<span class='alert-danger'>Password and confirmation don't match.</span>");
+                }
+                else{
+                  location.reload();
+                  // TODO: Connexion automatique
+                }
+            }
+          });
+         return false;
+        });
+        //Barre de recherche
         $('#q').val('');
         $('#q').keyup( function(){
           $field = $(this);
@@ -116,7 +207,7 @@ session_start();
             url : 'traitements/ajax-search.php',
             data : 'q='+$(this).val(),
             success : function(data){
-              console.log(data);
+              //S'il y a des résultats on affiche la timeline correspondante
               if(data != "No result"){
                 $('#ajax-loader').remove();
                 timeline = new TL.Timeline('timeline-embed',
@@ -129,6 +220,7 @@ session_start();
             });
             $('#clean').show();
           }
+          //On affiche la timeline par défaut
           else{
             timeline = new TL.Timeline('timeline-embed',
               'traitements/timeline.json', additionalOptions);
